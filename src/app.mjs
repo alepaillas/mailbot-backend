e40fa79c6f433f78d0654099c509ed4c0bd19e98
alepaillas <alepaillas@gmail.com>
2024-06-28 13:15:54 -0400

    update

--- src/app.mjs
+++ src/app.mjs
@@ -68,6 +68,37 @@ app.post("/send-to-ollama", async (req, res) => {
   }
 });
 
+app.get("/process-email", async (req, res) => {
+  try {
+    const emailText = await readMail();
+    //const prompt = "¿Puedes generar una respuesta de texto a este email?: ";
+    const prompt = "¿Me das los nombres de las personas involucradas en esta conversación?: ";
+    const combinedMessage = prompt + emailText; // Combine prompt and email text
+
+    // Call the /send-to-ollama route using its path and pass the combinedMessage as a parameter
+    const ollamaResponse = await axios.post(
+      "http://localhost:3000/send-to-ollama",
+      {
+        message: combinedMessage,
+      }
+    );
+
+    if (ollamaResponse.status === 200) {
+      res.json({
+        message: combinedMessage,
+        ollamaResponse: ollamaResponse.data,
+      });
+    } else {
+      res
+        .status(ollamaResponse.status)
+        .json({ error: ollamaResponse.data.message });
+    }
+  } catch (error) {
+    console.error("Error processing email:", error);
+    res.status(500).json({ error: "Error processing email" });
+  }
+});
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is listening on port ${port}`);
 });
